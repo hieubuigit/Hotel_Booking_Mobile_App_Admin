bc1fd4ec07f2d00a5c2b61f82fcb96e4eea4c3ac
Thành Phát <32811788+thanhphat147@users.noreply.github.com>
2020-11-11 19:16:09 +0700

    Update 11/11 L2
    Layout Menu

--- app/src/main/java/com/chuyende/hotelbookingappofadmin/DangNhap.java
+++ app/src/main/java/com/chuyende/hotelbookingappofadmin/DangNhap.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainDangNhap extends AppCompatActivity {
+public class DangNhap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
