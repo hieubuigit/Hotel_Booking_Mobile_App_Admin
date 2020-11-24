eaf656bea0d1dfeacb37b59d01048b00c3fb74f8
Thành Phát <32811788+thanhphat147@users.noreply.github.com>
2020-11-24 21:09:44 +0700

    24/11/2020
    chức năng đăng nhập

--- app/src/main/java/com/chuyende/hotelbookingappofadmin/ui/DangNhap.java
+++ app/src/main/java/com/chuyende/hotelbookingappofadmin/ui/DangNhap.java
@@ -1,5 +1,6 @@
 package com.chuyende.hotelbookingappofadmin.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +8,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.chuyende.hotelbookingappofadmin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.Document;
 
 public class DangNhap extends AppCompatActivity {
 
     Button btnDangNhap;
     EditText edtTenDangNhap;
     EditText edtMatKhau;
+    FirebaseFirestore firestore;
+    DocumentReference documentReference;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +37,51 @@ public class DangNhap extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         setControl();
         setEvent();
+        firestore = FirebaseFirestore.getInstance();
+        documentReference = firestore.collection("TaiKhoanAdmin").document();
     }
 
     private void setEvent() {
-        btnDangNhap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DangNhap.this, Menu.class);
-                startActivity(intent);
+        btnDangNhap.setOnClickListener(v -> {
+            String tenDangNhap = edtTenDangNhap.getText().toString();
+            String matKhau = edtMatKhau.getText().toString();
+
+            if (tenDangNhap.isEmpty()) {
+                edtTenDangNhap.setError("Vui lòng nhập tên đăng nhập của bạn");
+                edtTenDangNhap.requestFocus();
+            } else if (matKhau.isEmpty()) {
+                edtMatKhau.setError("Vui lòng nhập mật khẩu");
+                edtMatKhau.requestFocus();
+            } else if (tenDangNhap.isEmpty() && matKhau.isEmpty()) {
+                Toast.makeText(DangNhap.this, "Vui lòng đăng nhập", Toast.LENGTH_SHORT).show();
+            } else if (!(tenDangNhap.isEmpty() && !matKhau.isEmpty())) {
+                firestore.collection("TaiKhoanAdmin").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                String username = edtTenDangNhap.getText().toString().trim();
+                                String pass = edtMatKhau.getText().toString().trim();
+                                if (doc.getString("tenTaiKhoan").equalsIgnoreCase(username) && doc.getString("matKhau").equalsIgnoreCase(pass)) {
+                                    Toast.makeText(DangNhap.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                                    Intent intoMenu = new Intent(DangNhap.this, Menu.class);
+                                    startActivity(intoMenu);
+                                }
+                                else {
+                                    Toast.makeText(DangNhap.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                        else {
+                            Toast.makeText(DangNhap.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(DangNhap.this, "Có lỗi xảy ra", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     private void setControl() {
--- app/src/main/java/com/chuyende/hotelbookingappofadmin/ui/khachsan/FragmentKhachSan.java
+++ app/src/main/java/com/chuyende/hotelbookingappofadmin/ui/khachsan/FragmentKhachSan.java
@@ -1,5 +1,6 @@
 package com.chuyende.hotelbookingappofadmin.ui.khachsan;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,15 +14,19 @@ import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.chuyende.hotelbookingappofadmin.R;
+import com.chuyende.hotelbookingappofadmin.ui.DangNhap;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FragmentKhachSan extends Fragment {
 
-
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     private KhachSanViewModel khachSanViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -38,8 +43,18 @@ public class FragmentKhachSan extends Fragment {
         btnDangXuat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), ChiTietKhachSan.class);
-                startActivity(intent);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Thông báo");
+                builder.setMessage("Bạn có muốn thoát không ?");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intoDangNhap = new Intent(getActivity(), DangNhap.class);
+                        startActivity(intoDangNhap);
+                    }
+                });
+
             }
         });
 
