e4d855eb5634ae06d8abdd3130cf27e3b0e5b591
Thành Phát <32811788+thanhphat147@users.noreply.github.com>
2020-12-21 00:33:59 +0700

    Update ChiTietKhachSan.java

--- app/src/main/java/com/chuyende/hotelbookingappofadmin/ui/khachsan/ChiTietKhachSan.java
+++ app/src/main/java/com/chuyende/hotelbookingappofadmin/ui/khachsan/ChiTietKhachSan.java
@@ -1,9 +1,11 @@
 package com.chuyende.hotelbookingappofadmin.ui.khachsan;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -21,7 +23,13 @@ import com.chuyende.hotelbookingappofadmin.data_model.KhachSan;
 import com.chuyende.hotelbookingappofadmin.data_model.NguoiDung;
 import com.chuyende.hotelbookingappofadmin.data_model.Phong;
 import com.chuyende.hotelbookingappofadmin.data_model.ThongKeLuotDat;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -31,6 +39,8 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.UploadTask;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -48,11 +58,10 @@ public class ChiTietKhachSan extends AppCompatActivity {
     Phong phong;
     DaDat daDat;
     ThongKeLuotDat thongKeLuotDat;
-    ArrayList<KhachSan> dataKhachSan;
     ArrayList<Phong> dataPhong;
-    ArrayList<ThongKeLuotDat> dataThongKeLuotDat;
     ArrayList<DaDat> dataDaDat;
-    int tongLuotDat = 0;
+    ArrayList<ThongKeLuotDat> dataThongKeLuotDat;
+    String maKhachSan;
     private static String KHACHSAN = "KhachSan";
     private static String PHONG = "Phong";
     private static String DADAT = "DaDat";
@@ -65,15 +74,13 @@ public class ChiTietKhachSan extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.layout_chitietkhachsan);
+        maKhachSan = getIntent().getExtras().getString("maks");
+        LayDataKS(maKhachSan);
         setControl();
-        setEvent();
+        onStart();
+
     }
 
-    private void setEvent() {
-        String maKhachSan = getIntent().getExtras().getString("maks");
-        LayDataKS(maKhachSan);
-        LayThongKeLuotDat(maKhachSan);
-    }
 
     private void setControl() {
         imgAnhDaiDienKS = findViewById(R.id.imgAnhKhachSan);
@@ -102,6 +109,17 @@ public class ChiTietKhachSan extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        LayThongKeLuotDatBieuDo(maKhachSan);
+        swBieuDo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                } else {
+                    LayThongKeLuotDatBieuDo(maKhachSan);
+                }
+            }
+        });
     }
 
     @Override
@@ -127,18 +145,17 @@ public class ChiTietKhachSan extends AppCompatActivity {
         });
     }
 
-    public void LayThongKeLuotDat(String maKS) {
-        dataThongKeLuotDat = new ArrayList<>();
+    public void LayThongKeLuotDatBieuDo(String maKS) {
         db.collection(KHACHSAN).document(maKS).addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
                 if (value.exists()) {
                     khachSan = value.toObject(KhachSan.class);
                     khachSan.setMaKhachSan(value.getId());
-                    Log.d(TAG, "Lấy dữ liệu thành công");
                     db.collection(PHONG).whereEqualTo("maKhachSan", khachSan.getMaKhachSan()).addSnapshotListener(new EventListener<QuerySnapshot>() {
                         @Override
                         public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
+                            dataPhong = new ArrayList<>();
                             for (DocumentSnapshot doc : value) {
                                 if (e != null) {
                                     Toast.makeText(ChiTietKhachSan.this, "Có lỗi khi load dữ liệu phòng", Toast.LENGTH_SHORT).show();
@@ -154,15 +171,40 @@ public class ChiTietKhachSan extends AppCompatActivity {
                                 db.collection(DADAT).whereEqualTo("maPhong", p.getMaPhong()).addSnapshotListener(new EventListener<QuerySnapshot>() {
                                     @Override
                                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
-                                        if (e != null) {
-                                            Toast.makeText(ChiTietKhachSan.this, "Có lỗi khi load dữ liệu collection đã đặt", Toast.LENGTH_SHORT).show();
-                                            Log.d(TAG, e.toString());
-                                            return;
-                                        }
+                                        dataDaDat = new ArrayList<>();
                                         for (DocumentSnapshot doc : value) {
-                                            if (doc.exists()) {
-                                                
+                                            if (e != null) {
+                                                Toast.makeText(ChiTietKhachSan.this, "Có lỗi khi load dữ liệu collection đã đặt", Toast.LENGTH_SHORT).show();
+                                                Log.d(TAG, e.toString());
+                                                return;
                                             }
+                                            if (doc.exists()) {
+                                                daDat = doc.toObject(DaDat.class);
+                                                daDat.setMaDat(doc.getId());
+                                                dataDaDat.add(daDat);
+                                            }
+                                        }
+                                        for (int i = 0; i < dataDaDat.size(); i++) {
+                                            dataThongKeLuotDat = new ArrayList<>();
+                                            if (dataDaDat.get(i).getNgayDatPhong())
+                                            int thang = Integer.parseInt(dd.getNgayDatPhong().substring(3, 5));
+                                            int nam = Integer.parseInt(dd.getNgayDatPhong().substring(6, 10));
+                                            int tongLuotDat = 0;
+                                            thongKeLuotDat.setSoLuotDatThanhCong(tongLuotDat);
+                                            thongKeLuotDat.setThangDatPhong(thang + "/" + nam);
+                                            ArrayList <BarEntry> entries = new ArrayList<>();
+                                            entries.add(new BarEntry(thongKeLuotDat.getSoLuotDatThanhCong()), i);
+                                            BarDataSet dataSet = new BarDataSet(entries, "Dữ liệu đã đặt");
+                                            BarData data = new BarData(dataSet);
+                                            chart.setData(data);
+                                            data.setValueTextColor(Color.BLUE);
+                                            dataSet.setBarShadowColor(Color.WHITE);
+                                            dataSet.setValueTextSize(15);
+                                            chart.setDrawBarShadow(true);
+                                            dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                                            chart.animateY(3000, Easing.EaseInOutBounce);
+                                            chart.invalidate();
+
                                         }
                                     }
                                 });
@@ -170,27 +212,22 @@ public class ChiTietKhachSan extends AppCompatActivity {
                         }
                     });
                 }
-                if (error != null) {
-                    Toast.makeText(ChiTietKhachSan.this, "Có lỗi khi load dữ liệu khách sạn", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, e.toString());
-                    return;
-                }
             }
         });
     }
 
-    public void addThongKeLuotDat(ThongKeLuotDat tkld) {
+    public void addThongKeLuotDat(ThongKeLuotDat tkld, String key) {
         Map<String, Object> map = new HashMap<>();
         map.put("maKhachSan", tkld.getMaKhachSan());
-        map.put("tenKhachSan", tkld.getMaKhachSan());
+        map.put("tenKhachSan", tkld.getTenKhachSan());
         map.put("soLuotDatThanhCong", tkld.getSoLuotDatThanhCong());
         map.put("thangDatPhong", tkld.getThangDatPhong());
-        db.collection(TKLD).add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection(TKLD).document(key).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Log.d(TAG, "dữ liệu đã được thêm");
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "đã upload dữ liệu thống kê đã đặt phòng");
             }
         });
-    }
 
+    }
 }
